81894a9b58036a47e9064b38d4cb3aee9c38f565
JosenildoV <jva@cin.ufpe.br>
2019-11-30 16:24:50 -0300

    Mudanças no arquivo arduino

--- teste/teste.ino
+++ teste/teste.ino
@@ -1,23 +1,19 @@
 #include <ax12.h>
 #include <BioloidController.h>
 
-#define ombro1 2
-#define ombro2 3
-#define cotovelo 4
-#define pulso 5
-#define garra 6
-
 void setup(){ 
   Serial.begin(9600);
   delay(3000);
   Serial.println("start");
+  //Serial.read();
 }
 
 void loop(){
-  receiveComandsSerial();
+  receiveComandsTest();
 }
 
-void receiveComandsSerial(){  
+void receiveComandsTest()
+{  
     int motor2 = 0;
     int motor3 = 0;
     int motor4 = 0;
@@ -85,59 +81,59 @@ void receiveComands(){
   }
 }
 
-void samplePosition(){
+void sample(){
   SetPositionMulti(-7,47,27,49, 0);
 }
 
-void positionZeroRobot(){
+void moveRobot(){
   SetPositionMulti(0,0,0,0,0);
 }
 
 void catchObject(){
-  SetPositionExt(garra,-110);
+  SetPositionExt(6,-110);
 }
 
 void dropObject(){
-    SetPositionExt(garra,0);
+  SetPosition(6,degree2pos(0));
+  delay(3000);
 }
 
 void changeVelocity(){
   //Mudar velocidade
-
-  //torque
-  ax12SetRegister2(ombro1,14,100);
-  ax12SetRegister2(ombro2,14,800);
-  ax12SetRegister2(cotovelo,14,700);
-  ax12SetRegister2(pulso,14,400);
-  ax12SetRegister2(garra,14,500);
+  //ax12SetRegister2(2,14,100);
+  //ax12SetRegister2(3,14,800);
+  //ax12SetRegister2(4,14,700);
+  //ax12SetRegister2(5,14,400);
+  //ax12SetRegister2(6,14,500);
   
-  //velocidade
-  ax12SetRegister2(ombro1,32,80);
-  ax12SetRegister2(ombro2,32,80);
-  ax12SetRegister2(cotovelo,32,80);
-  ax12SetRegister2(pulso,32,80);
-  ax12SetRegister2(garra,32,80);
+  //ax12SetRegister2(2,32,80);
+  //ax12SetRegister2(3,32,80);
+  //ax12SetRegister2(4,32,80);
+  //ax12SetRegister2(5,32,80);
+  //ax12SetRegister2(6,32,80);
   
-  ax12SetRegister2(ombro1,33,0);
-  ax12SetRegister2(ombro2,33,0);
-  ax12SetRegister2(cotovelo,33,0);
-  ax12SetRegister2(pulso,33,0);
-  ax12SetRegister2(garra,33,0);
+  //ax12SetRegister2(2,33,0);
+  //ax12SetRegister2(3,33,0);
+  //ax12SetRegister2(4,33,0);
+  //ax12SetRegister2(5,33,0);
+  //ax12SetRegister2(6,33,0);
 }
 
 void turnOffRobot(){
  //'Desligar' o robo
-  ax12SetRegister(ombro1,AX_TORQUE_ENABLE,0);
-  ax12SetRegister(ombro2,AX_TORQUE_ENABLE,0);
-  ax12SetRegister(cotovelo,AX_TORQUE_ENABLE,0);
-  ax12SetRegister(pulso,AX_TORQUE_ENABLE,0);
-  ax12SetRegister(garra,AX_TORQUE_ENABLE,0);
+  ax12SetRegister(1,AX_TORQUE_ENABLE,0);
+  ax12SetRegister(2,AX_TORQUE_ENABLE,0);
+  ax12SetRegister(3,AX_TORQUE_ENABLE,0);
+  ax12SetRegister(4,AX_TORQUE_ENABLE,0);
+  ax12SetRegister(5,AX_TORQUE_ENABLE,0);
+  ax12SetRegister(6,AX_TORQUE_ENABLE,0);
   
-  ax12SetRegister(ombro1,AX_LED,0);
-  ax12SetRegister(ombro2,AX_LED,0);
-  ax12SetRegister(cotovelo,AX_LED,0);
-  ax12SetRegister(pulso,AX_LED,0);
-  ax12SetRegister(garra,AX_LED,0); 
+  ax12SetRegister(1,AX_LED,0);
+  ax12SetRegister(2,AX_LED,0);
+  ax12SetRegister(3,AX_LED,0);
+  ax12SetRegister(4,AX_LED,0);
+  ax12SetRegister(5,AX_LED,0);
+  ax12SetRegister(6,AX_LED,0); 
 }
 
 //Função para colocar braço na posição inicial
@@ -160,7 +156,8 @@ float pos2degree(int pos){
 }
 
 // Levar de grau para posição
-int degree2pos(float degree){
+int degree2pos(float degree)
+{
   if (degree<-150)
     degree = -150;
   if (degree>150)
@@ -173,15 +170,15 @@ int degree2pos(float degree){
 //Função para imprimir posição atual dos motores do braço
 void printPosition(){
   Serial.print("Ombro1: ");
-  Serial.println(GetPositionExt(ombro1));
+  Serial.println(GetPositionExt(2));
   Serial.print("Ombro2: ");
-  Serial.println(GetPositionExt(ombro2));
+  Serial.println(GetPositionExt(3));
   Serial.print("Cotovelo: ");
-  Serial.println(GetPositionExt(cotovelo));
+  Serial.println(GetPositionExt(4));
   Serial.print("Pulso: ");
-  Serial.println(GetPositionExt(pulso));
+  Serial.println(GetPositionExt(5));
   Serial.print("Garra: ");
-  Serial.println(GetPositionExt(garra));
+  Serial.println(GetPositionExt(6));
 }
 
 float GetPositionExt(int num_motor){
@@ -209,31 +206,29 @@ void SetPositionExt(int num_motor, float num_pos){
 
 // move todos os braços de uma vez, espera todos chegarem à posição esperada antes de continuar
 void SetPositionMulti(float num_pos1, float num_pos2, float num_pos3, float num_pos4, float num_pos5){
-  SetPosition(ombro1, degree2pos(num_pos1));
-  SetPosition(ombro2, degree2pos(num_pos2));
-  SetPosition(cotovelo, degree2pos(num_pos3));
-  SetPosition(pulso, degree2pos(num_pos4));
-  SetPosition(garra, degree2pos(num_pos5));
+  SetPosition(2, degree2pos(num_pos1));
+  SetPosition(3, degree2pos(num_pos2));
+  SetPosition(4, degree2pos(num_pos3));
+  SetPosition(5, degree2pos(num_pos4));
+  SetPosition(6, degree2pos(num_pos5));
   
   int i=0;
   
-  while(((GetPositionExt(ombro1) < num_pos1 -5 || GetPositionExt(ombro1) > num_pos1 +5) || (GetPositionExt(ombro2) < num_pos2 -5 || GetPositionExt(ombro2) > num_pos2 +5) || (GetPositionExt(cotovelo) < num_pos3 -5 || GetPositionExt(cotovelo) > num_pos3 +5) || (GetPositionExt(pulso) < num_pos4 -5 || GetPositionExt(pulso) > num_pos4 +5) || (GetPositionExt(garra) < num_pos5 -5 || GetPositionExt(garra) > num_pos5 +5)) && i<50){ 
+  while(((GetPositionExt(2) < num_pos1 -5 || GetPositionExt(2) > num_pos1 +5) || (GetPositionExt(3) < num_pos2 -5 || GetPositionExt(3) > num_pos2 +5) || (GetPositionExt(4) < num_pos3 -5 || GetPositionExt(4) > num_pos3 +5) || (GetPositionExt(5) < num_pos4 -5 || GetPositionExt(5) > num_pos4 +5) || (GetPositionExt(6) < num_pos5 -5 || GetPositionExt(6) > num_pos5 +5)) && i<50){ 
      
-    Serial.print((GetPositionExt(ombro1) < num_pos1 -5 || GetPositionExt(ombro1) > num_pos1 +5));
-    Serial.print((GetPositionExt(ombro2) < num_pos2 -5 || GetPositionExt(ombro2) > num_pos2 +5));
-    Serial.print((GetPositionExt(cotovelo) < num_pos3 -5 || GetPositionExt(cotovelo) > num_pos3 +5));
-    Serial.print((GetPositionExt(pulso) < num_pos4 -5 || GetPositionExt(pulso) > num_pos4 +5));
-    Serial.println((GetPositionExt(garra) < num_pos5 -5 || GetPositionExt(garra) > num_pos5 +5));
+    Serial.print((GetPositionExt(2) < num_pos1 -5 || GetPositionExt(2) > num_pos1 +5));
+    Serial.print((GetPositionExt(3) < num_pos2 -5 || GetPositionExt(3) > num_pos2 +5));
+    Serial.print((GetPositionExt(4) < num_pos3 -5 || GetPositionExt(4) > num_pos3 +5));
+    Serial.print((GetPositionExt(5) < num_pos4 -5 || GetPositionExt(5) > num_pos4 +5));
+    Serial.println((GetPositionExt(6) < num_pos5 -5 || GetPositionExt(6) > num_pos5 +5));
     delay(100);
     i++;
   }
-
   if(i>=50){
     Serial.println("Entrou outro loop!!!!");
     SetPositionMulti(num_pos1,num_pos2,num_pos3,num_pos4,num_pos5);
     return;
   }
-
   Serial.println("done.");
   delay(300);
   printPosition();
